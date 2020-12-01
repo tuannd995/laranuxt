3afbef2598c6018369186e633d160643e7a11873
tuannd <tuan.nd@zinza.com.vn>
2020-12-01 12:45:53 +0700

    integrate laravel and nuxt element-ui for client

---
+++ client/pages/admin/settings.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>settings</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: "Settings",
+  layout: "admin/layout",
+};
+</script>
--- client/plugins/element-ui.js
+++ client/plugins/element-ui.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import 'normalize.css/normalize.css'
 import Element from 'element-ui'
 import locale from 'element-ui/lib/locale/lang/en'
 
